f72ec063f80145c7ff89ca8f91a6609408f0d046
LIU-ZVen <zliudr@connect.ust.hk>
2025-12-12 08:50:46 +0800

    feat: add get_cuda_info but not bind, fix some bugs

--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -1,11 +1,15 @@
+#include <pybind11/pybind11.h>
 #include <torch/library.h>
 
 #include "kernels.h"
+#include "tools.h"
 #include "utils.h"
 
 TORCH_LIBRARY_WRAPPER(TORCH_LIBRARY_NAME, ops) {
   ops.def("hello_gpu", &hello_gpu);
   ops.impl("hello_gpu", torch::kCUDA, &hello_gpu);
 }
+
+// TODO(LIU-ZVen): bind some tools like `get_cuda_info`
 
 REGISTER_EXTENSION(TORCH_LIBRARY_NAME)
---
+++ csrc/include/tools.h
@@ -0,0 +1,8 @@
+#ifndef CSRC_INCLUDE_TOOLS_H_
+#define CSRC_INCLUDE_TOOLS_H_
+
+#include <torch/all.h>
+
+void get_cuda_info();
+
+#endif  // CSRC_INCLUDE_TOOLS_H_
