d91b7f147a8baf955b853a36e6622395086ee299
LIU-ZVen <zliudr@connect.ust.hk>
2025-12-14 19:06:46 +0800

    feat: add relu kernel

--- csrc/bindings.cpp
+++ csrc/bindings.cpp
@@ -8,6 +8,12 @@
 TORCH_LIBRARY_WRAPPER(TORCH_LIBRARY_NAME, ops) {
   ops.def("hello_gpu", &hello_gpu);
   ops.impl("hello_gpu", torch::kCUDA, &hello_gpu);
+
+  ops.def("elementwise_add(Tensor a, Tensor b) -> Tensor", &elementwise_add);
+  ops.impl("elementwise_add", torch::kCUDA, &elementwise_add);
+
+  ops.def("relu(Tensor input) -> Tensor", &relu);
+  ops.impl("relu", torch::kCUDA, &relu);
 }
 
 // TODO(LIU-ZVen): bind some tools like `get_cuda_info`
--- vcl/hello_world.py
+++ vcl/hello_world.py
@@ -1,8 +1,27 @@
+from vcl import vcl_C
+import torch
+
 def hello_world():
     print("Hello, World!")
 
 
-from vcl import vcl_C
-import torch
+hello_world()
 
 torch.ops.vcl_C.hello_gpu()
+
+x = torch.randn(5, device="cuda:0")
+y = torch.randn(5, device="cuda:0")
+
+print(f"x: {x}")
+print(f"y: {y}")
+
+z = torch.ops.vcl_C.elementwise_add(x, y)
+print(f"elementwise_add result: {z}")
+
+relu = torch.ops.vcl_C.relu(x)
+print(f"relu result: {relu}")
+
+x_f16 = x.to(torch.float16)
+print(f"x_f16: {x_f16}")
+relu_f16 = torch.ops.vcl_C.relu(x_f16)
+print(f"relu_f16 result: {relu_f16}")
